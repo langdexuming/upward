ec69cd4703cfb6d7501a8df7e137e26600f397b4
langdexuming_windows <langdexuming_x@163.com>
2017-12-03 13:48:11 +0800

    create Server and Client

--- DemoProject/Client/Program.cs
+++ DemoProject/Client/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace Client
 {
@@ -7,6 +10,97 @@ namespace Client
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            // var server = "120.79.0.192";
+            //var server = Dns.GetHostName();
+            var server = "127.0.0.1";
+            var port = 5200;
+
+            string result = SocketSendReceive(server, port);
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+
+        private static Socket ConnectSocket(IPAddress serverAddress, int port)
+        {
+            Socket s = null;
+
+            IPEndPoint ipe = new IPEndPoint(serverAddress, port);
+            Socket tempSocket =
+                new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            tempSocket.Connect(ipe);
+
+            if (tempSocket.Connected)
+            {
+                s = tempSocket;
+            }
+
+            return s;
+        }
+
+        private static Socket ConnectSocket(string server, int port)
+        {
+            Socket s = null;
+            IPHostEntry hostEntry = null;
+
+            // Get host related information.
+            hostEntry = Dns.GetHostEntry(server);
+
+            // Loop through the AddressList to obtain the supported AddressFamily. This is to avoid
+            // an exception that occurs when the host IP Address is not compatible with the address family
+            // (typical in the IPv6 case).
+            foreach (IPAddress address in hostEntry.AddressList)
+            {
+                s = ConnectSocket(address, port);
+                if (s != null)
+                {
+                    break;
+                }
+            }
+
+            return s;
+        }
+
+        // This method requests the home page content for the specified server.
+        private static string SocketSendReceive(string server, int port)
+        {
+            string request = "GET / HTTP/1.1\r\nHost: " + server +
+                "\r\nConnection: Close\r\n\r\n";
+            Byte[] bytesSent = Encoding.ASCII.GetBytes(request);
+            Byte[] bytesReceived = new Byte[256];
+
+            Socket s = null;
+            // Create a socket connection with the specified server and port.
+            //Socket s = ConnectSocket(server, port);
+            if (IPAddress.TryParse(server, out IPAddress serverAddress))
+            {
+                s = ConnectSocket(serverAddress, port);
+            }
+
+
+            if (s == null)
+                return ("Connection failed");
+
+            // Send request to the server.
+            s.Send(bytesSent, bytesSent.Length, 0);
+
+            // Receive the server home page content.
+            int bytes = 0;
+            string page = "Default HTML page on " + server + ":\r\n";
+
+            // The following will block until te page is transmitted.
+            do
+            {
+                bytes = s.Receive(bytesReceived, bytesReceived.Length, 0);
+                page = page + Encoding.ASCII.GetString(bytesReceived, 0, bytes);
+            }
+            while (bytes > 0);
+
+            return page;
         }
     }
+
+
 }
--- DemoProject/Server/Program.cs
+++ DemoProject/Server/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace Server
 {
@@ -7,6 +10,33 @@ namespace Server
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            //var server = Dns.GetHostName();
+            var server = "127.0.0.1";
+
+            var port = 5200;
+
+            Socket s = null;
+
+            // IPHostEntry hostEntry = null;
+
+            // // Get host related information.
+            // hostEntry = Dns.GetHostEntry(server);
+
+            s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            s.Bind(new IPEndPoint(IPAddress.Parse(server), port));
+            s.Listen(1);
+            var csFd = s.Accept();
+            Console.WriteLine(csFd.RemoteEndPoint.ToString());
+
+            byte[] buffer = new byte[2500];
+            csFd.Receive(buffer);
+            var str = Encoding.ASCII.GetString(buffer);
+            Console.WriteLine(str);
+
+            Console.ReadLine();
+
+            s.Shutdown(SocketShutdown.Both);
         }
     }
 }
