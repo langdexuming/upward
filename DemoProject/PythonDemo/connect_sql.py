0a5a80e38bf1d93bd117f189e9d4d609340f671d
langdexuming_linux_server <langdexuming_x@163.com>
2018-01-04 00:35:36 +0800

    publish

--- DemoProject/PythonDemo/connect_sql.py
+++ DemoProject/PythonDemo/connect_sql.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
 
 # connect to mysql
-
-from common.mysql_client import *
-from pymssql import *
+import mysql.connector
 
 def connect_mysql():
-    print("no implement! ")	
+	cnx=mysql.connector.connect(user='root',database='upward_api_test')
+
+    curA=cnx.cursor(buffered=True)
+
+	print("connect ok")
+
+	cnx.close()
+	# print("no implement! ")	
 	
 def connect_sqlserver():
-    return pymssql.connect()
+    print("no implement! ")	
 
 def connect_sqlite():
     print("no implement! ")	
 
     
 # main
-conn=connect_sqlserver()
+# conn=connect_sqlserver()
+cnx=connect_mysql()
 
 
