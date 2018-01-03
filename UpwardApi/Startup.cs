20778f710e0f115427026468530553c4c47b602d
langdexuming_windows <langdexuming_x@163.com>
2018-01-04 00:39:10 +0800

    补充提交

--- UpwardApi/Startup.cs
+++ UpwardApi/Startup.cs
@@ -27,7 +27,7 @@ namespace UpwardApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-#if false
+#if true
             services.AddDbContext<TodoContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 #else
             services.AddDbContext<TodoContext>(opt => opt.UseMySQL(Configuration.GetConnectionString("DefaultConnection")));
